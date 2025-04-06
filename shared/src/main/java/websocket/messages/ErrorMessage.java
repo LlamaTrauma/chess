e935242353ad7ec1b9e889353eb293c62c76ca40
LlamaTrauma <44646755+LlamaTrauma@users.noreply.github.com>
2025-04-06 00:32:00 -0600

    asdasd
    asdasa

---
+++ shared/src/main/java/websocket/messages/ErrorMessage.java
@@ -0,0 +1,10 @@
+package websocket.messages;
+
+public class ErrorMessage extends ServerMessage {
+    public final String message;
+
+    public ErrorMessage (String message) {
+        super(ServerMessageType.ERROR);
+        this.message = message;
+    }
+}
--- shared/src/main/java/websocket/messages/LoadGameMessage.java
+++ shared/src/main/java/websocket/messages/LoadGameMessage.java
@@ -4,7 +4,7 @@ import chess.ChessGame;
 
 public class LoadGameMessage extends ServerMessage {
     public final ChessGame game;
-    
+
     public LoadGameMessage(ChessGame game) {
         super(ServerMessageType.LOAD_GAME);
         this.game = game;
